e8a6779f9307bfd970644de39cc33fad05a2633f
ChristopherBz <81110745+ChristopherBz@users.noreply.github.com>
2021-08-24 14:46:41 +1000

    db update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,17 @@
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Michael', 'Sott', 1, null);
+VALUES ('Jack', 'Riely', 1, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Jim', 'Halpert', 2, 1);
+VALUES ('Frank', 'Milton', 2, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Dwight', 'Schrute', 2, 1);
+VALUES ('Steve', 'Brown', 2, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Pam', 'Beasley', 4, 1);
+VALUES ('Josh', 'Beets', 4, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Kevin', 'Malone', 3, 1);
+VALUES ('Kevin', 'Bacon', 3, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Toby', 'Flannagan', 5, 2);
+VALUES ('Dobes', 'Flin', 5, 2);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Robert', 'California', 6, null);
+VALUES ('Matt', 'College', 6, null);
 
 INSERT INTO department (department_name)
 VALUES ('Management');
